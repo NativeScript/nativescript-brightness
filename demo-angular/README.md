c916e83cd038cf87dba57378c3a9edbe0a9dcf7c
Elena Hristova <elena.hristova@progress.com>
2018-11-26 13:20:40 +0200

    add angular demo

---
+++ demo-angular/README.md
@@ -0,0 +1,9 @@
+# Example for using nativescript-brightness plugin
+## This example demonstrates how to use plugin with nativescript-angular and webpack
+
+If you want to test it out on an emulator or a device you can follow the instructions below:
+
+`git clone https://github.com/NativeScript/nativescript-brightness.git`  
+`cd nativescript-brightness/demo` or `cd nativescript-brightness/demo-angular`  
+`npm run build.plugin && npm install`  
+`tns run android` or `tns run ios` depending on the platform you want to test
---
+++ demo-angular/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Brightness } from 'nativescript-brightness';
+import { Slider } from "tns-core-modules/ui/slider";
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app.component.html'
+})
+export class AppComponent {
+    public brightness = new Brightness();
+    public currentValue = this.brightness.get();
+    public sliderValue = this.currentValue;
+    public message = this.getMessage();
+    public onSliderValueChange(args) {
+        let slider = <Slider>args.object;
+
+        this.currentValue = slider.value;
+
+        let value = Math.round(slider.value);
+        this.brightness.set({ intensity: value });
+        this.currentValue = value;
+        this.message = this.getMessage();
+    }
+    public getMessage() {
+        return `Your screen brightness is ${this.currentValue}`;
+    }
+}
