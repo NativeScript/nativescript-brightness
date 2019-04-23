4a19aeb024d60b40b15dac4ca94b7955a1132d2e
Elena Hristova <elena.hristova@progress.com>
2019-04-23 11:51:52 +0300

    fix: demo-vue for android api level < 23

--- src/brightness.android.ts
+++ src/brightness.android.ts
@@ -16,9 +16,12 @@ export class Brightness {
         validateOptions(options);
 
         if (android.os.Build.VERSION.SDK_INT < 23) {
-            const attr = topmost().android.activity.getWindow().getAttributes();
-            attr.screenBrightness = options.intensity / 100;
-            topmost().android.activity.getWindow().setAttributes(attr);
+            const topFrame = topmost();
+            if (topFrame && topFrame.android && topFrame.android.activity) {
+                const attr = topFrame.android.activity.getWindow().getAttributes();
+                attr.screenBrightness = options.intensity / 100;
+                topFrame.android.activity.getWindow().setAttributes(attr);
+            }
         } else {
             let context = applicationModule.android.foregroundActivity;
             let brightnessValue = Math.round(options.intensity * 255 / 100);
